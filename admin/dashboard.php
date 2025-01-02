3cd9df8a7a1c5e39d674814a75c9aa39ba188e9a
Qoriah-19 <2201020096@student.umrah.ac.id>
2025-01-02 21:57:04 +0700

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,7 +3,7 @@ session_start();
 
 // Cek apakah admin sudah login
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-    header('Location: /otakuu/auth/login_admin.php');  // Redirect ke halaman login jika bukan admin
+    header('Location: ../auth/login_admin.php');  // Redirect ke halaman login jika bukan admin
     exit();
 }
 ?>
@@ -14,7 +14,7 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Admin</title>
-    <link rel="stylesheet" href="../assets/css/admin.css">
+    <link rel="stylesheet" href="../assets/css/dashboard.css">
     <script src="../assets/js/script.js" defer></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -28,43 +28,17 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
     <!-- Sidebar / Menu -->
     <nav>
         <ul class="nav-menu">
-            <!-- Menu untuk admin -->
-            <li><a href="index.php">Beranda</a></li>
-            <li><a href="index.php?page=profil_sungai_enam">Profil Sungai Enam</a></li>
+            
             <li>
-                <a href="#">Informasi</a>
-                <ul class="dropdown">
-                    <li><a href="index.php?page=info&topic=Toko">Toko</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Approve Mitra</a>
                 <ul class="dropdown">
                     <li><a href="approve_mitra.php">Approve Mitra</a></li>
                     <li><a href="approve_produk_mitra.php">Approve Produk</a></li>
+                    <li><a href="laporan.php">Laporan Mitra dan Produk</a>
+                    </li>
                 </ul>
             </li>
         </ul>
     </nav>
-
-    <!-- Main Content -->
-    <main>
-        <div class="welcome">
-            <h2>Selamat Datang, Admin!</h2>
-        </div>
-
-        <!-- Tindakan Admin -->
-        <div class="actions">
-            <div class="action-item">
-                <h3>Approve Mitra</h3>
-                <a href="approve_mitra.php">Approve Mitra</a>
-            </div>
-            <div class="action-item">
-                <h3>Approve Produk</h3>
-                <a href="approve_produk_mitra.php">Approve Produk</a>
-            </div>
-        </div>
-    </main>
 
     <!-- Footer -->
     <footer>
